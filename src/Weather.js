3e11aa55e0b347f3072dfb18f2c96541731ffcee
Sarah NC <76857484+snoelcameron@users.noreply.github.com>
2021-04-23 00:11:12 -0400

    added backgroundColor import

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherUpdate from "./WeatherUpdate";
+import backgroundColor from "./backgroundColor";
 import Forecast from "./Forecast";
 import "./App.css";
 
